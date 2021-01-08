55edb5f09ff962a189f7353fd2b6a0fdd00218d0
YesseniaChavez <75495566+YesseniaChavez@users.noreply.github.com>
2021-01-08 13:44:02 -0500

    WS01 Hello World in three programing languages

---
+++ workshops/unit1/WS01helloworld/Hello world.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+int main(){
+	int grade=20;
+	//printf() displays information
+	printf("Hello World from \t \" Yessenia Chavez! \" =) \n");
+	printf("\n The grade is %d \n", grade);
+	return0;
+}
