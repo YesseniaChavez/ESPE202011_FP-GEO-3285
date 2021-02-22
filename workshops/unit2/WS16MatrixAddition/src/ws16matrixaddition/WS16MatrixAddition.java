2e0ae39d298256ce0fa9f1492e435fbf62a3dc0a
YesseniaChavez <75495566+YesseniaChavez@users.noreply.github.com>
2021-02-22 15:50:39 -0500

    WS15SolutionQuiz

--- workshops/unit2/WS16MatrixAddition/src/ws16matrixaddition/WS16MatrixAddition.java
+++ workshops/unit2/WS16MatrixAddition/src/ws16matrixaddition/WS16MatrixAddition.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ws16matrixaddition;
 
-/**
- *
- * @author Yessi
- */
 public class WS16MatrixAddition {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int a[][] = {{6, 3, 5}, {3, 9, 10}, {1, -2, 4}};
         int b[][] = {{4, 10, 4}, {2, 1, 3}, {2, 8, 4}};
